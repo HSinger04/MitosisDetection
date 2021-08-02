b70808bf6ae1e8a5bf8f6ea3ff92d2dcb11b2ef5
HSinger04 <hermannsinger@gmx.net>
2021-08-02 11:55:13 +0000

    Create img_preprocessing.py

---
+++ src/utils/img_preprocessing.py
@@ -0,0 +1,23 @@
+from torchvision.transforms.functional import rotate
+
+def img2patches(rgb_img, patch_size, angle=270):
+    """
+
+    :param rgb_img: the image to split
+    :param patch_size: individual patches will be of size patch_size x patch_size
+    :param angle: the angle by which to rotate the patch image.
+                  Defaults to 270 for restoring original rotation
+    :return: the image patches
+    """
+    if not rgb_img.shape[-1] % patch_size == 0:
+        raise ValueError("patch_size must cleanly divide height and width")
+    
+    patches = rgb_img.unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size).unfold(3, patch_size, patch_size)
+    patches = patches.reshape((3, 2, 2, patch_size, patch_size))
+    # swap channel dim with row dim
+    patches = patches.transpose(0, 1)
+    # swap channel dim with col dim
+    patches = patches.transpose(1, 2)
+    patches = patches.reshape(-1, 3, patch_size, patch_size)
+    patches = rotate(patches, angle)
+    return patches
