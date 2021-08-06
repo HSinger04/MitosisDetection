7ead949cb98b66bb7eefbc718f4ca3140a985299
HSinger04 <hermannsinger@gmx.net>
2021-08-06 15:29:23 +0200

    Correct img_and_bboxes2patches

--- src/utils/img_preprocessing.py
+++ src/utils/img_preprocessing.py
@@ -29,7 +29,7 @@ def img2patches(rgb_img, patch_size, angle=270):
     return patches
 
 
-def img_and_bboxes2patches(rgb_img, bboxes, patch_size, angle=270):
+def img_and_bboxes2patches(rgb_img, bboxes, patch_size, angle=270):  
     """
     :param rgb_img: the image to split. Must be square
     :param bboxes: the bounding boxes of the image. Each entry is a quadruple
@@ -38,40 +38,40 @@ def img_and_bboxes2patches(rgb_img, bboxes, patch_size, angle=270):
     :param angle: the angle by which to rotate the patch image.
                   Defaults to 270 for restoring original rotation
     :return: the image patches and their bboxes, generated from right to left, top to bottom
-    """
+    """  
 
     if not rgb_img.shape[-1] % patch_size == 0:
         raise ValueError("patch_size must cleanly divide height and width")
 
-    img_patches = img2patches(rgb_img, patch_size, angle=angle)
+    img_patches = img2patches(rgb_img, patch_size, angle=angle) 
 
     # we get num_patches_root^2 many patches
     num_patches_root = rgb_img.shape[-1] // patch_size
     # list for storing bboxes for each patch
-    bboxes_to_patches = [[]] * (num_patches_root * num_patches_root)
+    bboxes_to_patches = [[] for _ in range(num_patches_root * num_patches_root)]
 
     for left_x, top_y, width, height in bboxes:
         # values help in determining in which image patch the four bbox coordinate points lie.
         right_x = left_x + width
         bottom_y = top_y + height
 
+        # says in which patch the respective coordinate lies.
         patch_left_x = left_x // patch_size
         patch_right_x = right_x // patch_size
         patch_top_y = top_y // patch_size
         patch_bottom_y = bottom_y // patch_size
-        # TODO: Make sure to store width and height, not right_x and bottom_y
 
         # Iterate through the relevant patches from left to right, top to bottom
         for patch_x in range(patch_left_x, patch_right_x + 1):
             for patch_y in range(patch_top_y, patch_bottom_y + 1):
-                # Init empty bbox current patch.  Also fill...
-                bbox_to_patch = [None] * 4
+                # Init empty bbox current patch.  Also fill... 
+                bbox_to_patch = [None for i in range(4)]
                 # with left-most x
                 if not patch_x == patch_left_x:
                     bbox_to_patch[0] = 0
                 else:
-                    bbox_to_patch[0] = left_x % patch_size
-                    # with top y
+                    bbox_to_patch[0] = left_x % patch_size 
+                # with top y 
                 if not patch_y == patch_top_y:
                     bbox_to_patch[1] = 0
                 else:
@@ -80,16 +80,14 @@ def img_and_bboxes2patches(rgb_img, bboxes, patch_size, angle=270):
                 if not patch_x == patch_right_x:
                     bbox_to_patch[2] = patch_size - 1 - bbox_to_patch[0]
                 else:
-                    bbox_to_patch[2] = (right_x % patch_size) - bbox_to_patch[0]
-                    # with height
+                    bbox_to_patch[2] = (right_x % patch_size) - bbox_to_patch[0] 
+                # with height
                 if not patch_y == patch_bottom_y:
-                    bbox_to_patch[3] = patch_size - 1 - bbox_to_patch[2]
+                    bbox_to_patch[3] = patch_size - 1 - bbox_to_patch[1]
                 else:
-                    bbox_to_patch[3] = (bottom_y % patch_size) - bbox_to_patch[2]
-
+                    bbox_to_patch[3] = (bottom_y % patch_size) - bbox_to_patch[1]
+                
                 # store the bbox
-                # TODO: remove
-                print(bboxes_to_patches)
                 bboxes_to_patches[patch_x + patch_y * num_patches_root].append(bbox_to_patch)
 
     return img_patches, bboxes_to_patches
