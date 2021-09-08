ce4f82468bb292af1ff9e5d47a27bfae69b05e77
HSinger04 <hermannsinger@gmx.net>
2021-09-08 10:55:37 +0200

    Work a bit on function body

--- preprocess_imgs_and_bboxes.py
+++ preprocess_imgs_and_bboxes.py
@@ -1,7 +1,7 @@
 import os
 
 def main(parent_dir, img_id, save_dir, rotate_angle=None, resize_size=None, patch_img_size=None):
-    """ Rotate, resize and patch images.
+    """ Rotate, resize and patch images. Also works for images without bboxes.
     
     :param parent_dir: Images in subdirectories of parent_dir are those to be preprocessed.
     :param img_id: Ending of image files that need to be preprocessed. E.g. if you want all images ending with ".png" to be preprocessed, set img_id = ".png".
@@ -11,4 +11,14 @@ def main(parent_dir, img_id, save_dir, rotate_angle=None, resize_size=None, patc
     :param patch_img_size: Size to which images should be patched. Leave out if you don't want images patched.
     """
     
+    subdirs = [x[0] for x in os.walk(dir)] 
+    for subdir in subdirs: 
+        files = os.walk(subdir).__next__()[2]
+        if (len(files) > 0):                                                                                          
+            for file_name in files: 
+                if file_name.endswith(img_id):
+                    src_path = os.path.join(subdir, file_name) 
+                    # Apply transforms and compose name that way.
+                    trsf_name = ""
+                    save_path = src_path[:-4] + end_txt + img_id[-4:]
     
